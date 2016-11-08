5704c6b22aac1bc6476d8d71bb1904e12f10f47a
alex <olefirenk@gmail.com>
2016-11-08 21:07:45 +0200

    apiconfig

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
     api:{
         baseUrl: 'http://sportspace.k-3soft.com:88/app_dev.php/api',
         methods:{
-            getUserData: '/accounts',
+            getUserData: '/accounts/',
             getConversation: (params)=>`/conversations/${params}`,
             postMessage: (params)=>`/conversations/${params}/messages/`
         }
